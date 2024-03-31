d54eff074e78e4c7e77330aa55e6ae149ea863c2
taotao <mrlazy1708@gmail.com>
2024-03-31 12:50:44 +0800

    add changes in a9ac686 back

--- src/lib.rs
+++ src/lib.rs
@@ -21,8 +21,8 @@ extern crate crossterm;
 use crossterm::QueueableCommand;
 use crossterm::{cursor, terminal};
 
-#[macro_use]
-extern crate lazy_static;
+extern crate once_cell;
+use once_cell::sync::Lazy;
 
 #[cfg(test)]
 mod test;
@@ -412,11 +412,9 @@ impl<Iter: Iterator> crate::Iter<Iter::Item> for Iter {}
 
 /* --------------------------------- STATIC --------------------------------- */
 
-lazy_static! {
-    static ref ID: sync::atomic::AtomicUsize = sync::atomic::AtomicUsize::new(0);
-    static ref BAR: sync::Mutex<collections::BTreeMap<usize, Info>> =
-        sync::Mutex::new(collections::BTreeMap::new());
-}
+static ID: sync::atomic::AtomicUsize = sync::atomic::AtomicUsize::new(0);
+static BAR: Lazy<sync::Mutex<collections::BTreeMap<usize, Info>>> =
+    Lazy::new(|| sync::Mutex::new(collections::BTreeMap::new()));
 
 fn size<T: From<u16>>() -> (T, T) {
     let (width, height) = crossterm::terminal::size().unwrap_or((80, 24));
