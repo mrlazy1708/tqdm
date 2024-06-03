02851bf26b2ab2a1fb87f876f60ad48ebdab4c15
AVS Origami <avs.origami@gmail.com>
2024-06-03 15:57:03 -0400

    Add Pacman style.

--- src/style.rs
+++ src/style.rs
@@ -3,6 +3,8 @@
 //! - `ASCII`: Pure ASCII bar with `"0123456789#"`
 //! - `Block`: Common bar with unicode characters `" ▏▎▍▌▋▊▉█"`
 //! - `Balloon`: Simulate balloon explosion with `".oO@*"`. Inspired by [stackoverflow](https://stackoverflow.com/a/2685509/17570263)
+//! - `Pacman`: Inspired by Arch Linux ILoveCandy
+//! - `Custom`: Create a custom progressbar style
 //!
 //! Other styles are open for [contribution](https://github.com/mrlazy1708/tqdm/issues/1).
 
@@ -10,6 +12,7 @@ pub enum Style {
     ASCII,
     Block,
     Balloon,
+    Pacman,
     Custom(String)
 }
 
@@ -25,6 +28,7 @@ impl ToString for Style {
             Style::ASCII => "0123456789#",
             Style::Block => " ▏▎▍▌▋▊▉█",
             Style::Balloon => ".oO@*",
+            Style::Pacman => "C-",
             Style::Custom(n) => &n[..],
         })
     }
